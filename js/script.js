d2e896ff12d712d542f6f6794de291250588348d
danielbosa <danielbosa95@gmail.com>
2024-04-05 23:51:01 +0200

    Fixed styling of last msg in open chats section: text ellipsis instead of wrapping

--- js/script.js
+++ js/script.js
@@ -128,8 +128,8 @@ createApp({
 
 /*
 Funzionalità
-- visualizzare nella lista dei contatti l'ultimo messaggio inviato/ricevuto da ciascun contatto
-- inserire l'orario corretto nei messaggi
+- sistemare spunte blu nella parte destra: si devono colorare di blu solo 500ms dopo invio;
+
 - sotto al nome del contatto nella parte in alto a destra, cambiare l'indicazione dello stato: visualizzare il testo "sta scrivendo..." nel timeout in cui il pc risponde, poi mantenere la scritta "online" per un paio di secondi e infine visualizzare "ultimo accesso alle xx:yy" con l'orario corretto
 - dare la possibilità all'utente di cancellare tutti i messaggi di un contatto o di cancellare l'intera chat con tutti i suoi dati: cliccando sull'icona con i tre pallini in alto a destra, si apre un dropdown menu in cui sono presenti le voci "Elimina messaggi" ed "Elimina chat"; cliccando su di essi si cancellano rispettivamente tutti i messaggi di quel contatto (quindi rimane la conversazione vuota) oppure l'intera chat comprensiva di tutti i dati del contatto oltre che tutti i suoi messaggi (quindi sparisce il contatto anche dalla lista di sinistra)
 - dare la possibilità all'utente di aggiungere una nuova conversazione, inserendo in un popup il nome e il link all'icona del nuovo contatto
